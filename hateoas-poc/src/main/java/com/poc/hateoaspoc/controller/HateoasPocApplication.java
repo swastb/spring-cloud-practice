48f97eba6c3cf369e55b4a538810145ee3a13ec5
swastika.basu@cognizant.com <swastika.basu@cognizant.com>
2017-12-19 00:42:13 +0530

    Adding swagger

--- hateoas-poc/src/main/java/com/poc/hateoaspoc/controller/HateoasPocApplication.java
+++ hateoas-poc/src/main/java/com/poc/hateoaspoc/controller/HateoasPocApplication.java
@@ -6,13 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 import com.poc.hateoaspoc.controller.model.Reservation;
 
-@SpringBootApplication
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
+@SpringBootApplication
+@EnableSwagger2
 @ComponentScan("com.poc.hateoaspoc")
 public class HateoasPocApplication {
 	
@@ -34,4 +41,11 @@ class SampleRecordsCLR implements CommandLineRunner {
 
 		// reservationRepository.findAll().forEach(System.out::println);
 	}
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()                 .apis(RequestHandlerSelectors.basePackage("com.poc.hateoaspoc.controller"))
+                .paths(regex("/customers.*"))
+                .build();
+    }
 }
